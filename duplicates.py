eace0602b176dc4c644952fe7664a4f7c771497c
Andrey Lebowski <ase@crimea.com>
2018-01-09 16:45:29 +0300

    read folder and add files attributes to list

--- duplicates.py
+++ duplicates.py
@@ -1,3 +1,23 @@
+import os
+
+
+def get_duplicate_files(path):
+
+    list_files = []
+
+    for path_dir, directories, files in os.walk(path):
+        for file_name in files:
+            full_path = os.path.join(path_dir, file_name)
+            file_size = os.path.getsize(full_path)
+            file_attributes = {
+                'file_name': file_name,
+                'file_size': file_size,
+                'file_full_path': full_path,
+            }
+            list_files.append(file_attributes)
+
 
 if __name__ == '__main__':
-    pass
+    path = 'D:\Temp'
+    duplicates = get_duplicate_files(path)
+
