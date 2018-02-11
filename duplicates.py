327b73ff19f72606409f31453620850233acf7ff
Andrey (home) <ase@crimea.com>
2018-02-11 14:30:53 +0300

    make find duplicates function

--- duplicates.py
+++ duplicates.py
@@ -1,23 +1,30 @@
 import os
+from collections import Counter
+from pprint import pprint
 
 
 def get_duplicate_files(path):
-
-    list_files = []
-
+    dict_files = {}
+    counter = Counter()
     for path_dir, directories, files in os.walk(path):
         for file_name in files:
             full_path = os.path.join(path_dir, file_name)
             file_size = os.path.getsize(full_path)
-            file_attributes = {
-                'file_name': file_name,
-                'file_size': file_size,
-                'file_full_path': full_path,
-            }
-            list_files.append(file_attributes)
+            key = (file_name, file_size)
+
+            file_paths = dict_files.get(key, [])
+            file_paths.append(full_path)
+            dict_files[key] = file_paths
+            counter[key] += 1
+
+    duplicates = [dict_files[key] for key, count in counter.items() if count > 1]
+    return duplicates
+
 
 
 if __name__ == '__main__':
-    path = 'D:\Temp'
+    path = 'D:/temp'
     duplicates = get_duplicate_files(path)
+    for el in duplicates:
+        print(el)
 
